5e9a2d9ff97af89e13a7016016bed695eaa42ca9
B.Geri <baligergely@gmail.com>
2023-06-08 14:05:15 +0200

    fix autoscaler and format

--- eks.tf
+++ eks.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "eks_cluster" {
-  name = "eks-cluster"
+  name               = "eks-cluster"
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -18,17 +18,17 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "amazon_eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role = aws_iam_role.eks_cluster.name
+  role       = aws_iam_role.eks_cluster.name
 }
 
 resource "aws_eks_cluster" "eks" {
-  name = "eks"
+  name     = "eks"
   role_arn = aws_iam_role.eks_cluster.arn
-  version = "1.25"
+  version  = "1.25"
 
   vpc_config {
     endpoint_private_access = false
-    endpoint_public_access = true
+    endpoint_public_access  = true
     subnet_ids = [
       aws_subnet.public_1.id,
       aws_subnet.public_2.id,
@@ -36,7 +36,7 @@ resource "aws_eks_cluster" "eks" {
       aws_subnet.private_2.id
     ]
   }
-  
+
   depends_on = [
     aws_iam_role_policy_attachment.amazon_eks_cluster_policy
   ]
