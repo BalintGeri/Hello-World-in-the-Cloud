a3d8c4f2b5959fcbff35522a184935aa8b6955b3
B.Geri <baligergely@gmail.com>
2023-06-20 10:30:02 +0200

    remove default values for subnets CIDR

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,8 @@
 region = "eu-north-1"
+VPC_CIDR_block = "192.168.0.0/16"
+public_1_subnet_CIDR = "192.168.0.0/18"
+public_2_subnet_CIDR = "192.168.64.0/18"
+private_1_subnet_CIDR = "192.168.128.0/18"
+private_2_subnet_CIDR = "192.168.192.0/18"
 availability_zones = ["eu-north-1b", "eu-north-1a"]
 eks_version = "1.25"
--- variables.tf
+++ variables.tf
@@ -8,29 +8,24 @@ variable "region" {
 
 variable "VPC_CIDR_block" {
   type = string
-  default = "192.168.0.0/16"
 }
 
 //subnets cidr 
 
 variable "public_1_subnet_CIDR" {
   type = string
-  default = "192.168.0.0/18"
 }
 
 variable "public_2_subnet_CIDR" {
   type = string
-  default = "192.168.64.0/18"
 }
 
 variable "private_1_subnet_CIDR" {
   type = string
-  default = "192.168.128.0/18"
 }
 
 variable "private_2_subnet_CIDR" {
   type = string
-  default = "192.168.192.0/18"
 }
 
 //av zones
