796d8abce2c15876ff9f0863017652223e72cb68
Geri <109275872+BalintGeri@users.noreply.github.com>
2023-07-04 15:28:11 +0200

    Update eks-node-groups.tf

--- eks-node-groups.tf
+++ eks-node-groups.tf
@@ -41,9 +41,9 @@ resource "aws_eks_node_group" "nodes_general" {
   ]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   update_config {
