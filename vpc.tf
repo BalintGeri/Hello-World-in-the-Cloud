9ffadb36c9b408e9ce32feafc687b226a7f71e6a
Geri <109275872+BalintGeri@users.noreply.github.com>
2023-08-04 08:07:37 +0200

    reverse demonstration commit

--- vpc.tf
+++ vpc.tf
@@ -8,14 +8,3 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
-
-resource "aws_vpc" "hello" {
-  cidr_block                       = "190.165.0.0/16"
-  instance_tenancy                 = "default"
-  enable_dns_support               = true
-  enable_dns_hostnames             = true
-  assign_generated_ipv6_cidr_block = false
-  tags = {
-    Name = "hello"
-  }
-}
